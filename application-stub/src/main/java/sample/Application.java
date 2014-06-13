8069a05d5083bf7a5fb52c3d77420192dd21d643
pcierpiatka <p.cierpiatka@gmail.com>
2014-06-14 00:17:41 +0200

    mongodb database name hack

--- application-stub/src/main/java/sample/Application.java
+++ application-stub/src/main/java/sample/Application.java
@@ -2,38 +2,36 @@ package sample;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import sample.domain.Event;
-import sample.repository.EventRepository;
-
-import java.util.List;
-
-import static sample.domain.builder.EventBuilder.event;
+import org.springframework.util.StringUtils;
 
 @ComponentScan(basePackageClasses = Application.class)
 @EnableAutoConfiguration
 @EnableMongoRepositories
 @Configuration
-public class Application extends SpringBootServletInitializer implements CommandLineRunner {
-
-    private static final Log LOGGER = LogFactory.getLog(Application.class);
+public class Application extends SpringBootServletInitializer {
 
     @Autowired
-    private EventRepository eventRepository;
+    private MongoProperties mongoProperties;
+
+    public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+        mongoProperties.setDatabase("letsMeetUp");
+        return new SimpleMongoDbFactory(new MongoClient(), mongoProperties.getDatabase());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -42,40 +40,5 @@ public class Application extends SpringBootServletInitializer implements Command
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-
-    public void init()  {
-
-        eventRepository.deleteAll();
-
-
-        for(int i = 0; i < 10 ; i ++) {
-
-            Event event = event()
-                    .withDescription("This is meeting in Bar number " + i)
-                    .withName("Let's meet again (nr ( " + i + ")")
-                    .withMeetUpDate(DateTime.now().plusDays(i).plusHours(i).plusMinutes(i))
-                    .withUser()
-                    .withName("Bożydar " + i)
-                    .withEmail("dar.boży." + i + "@rodzice.przegrali.zaklad.pl")
-                    .endUser()
-                    .withPlace()
-                    .withPlace("Super miejscowa " + i)
-                    .endPlace()
-                    .build();
-
-            eventRepository.save(event);
-
-        }
-
-        List<Event> events = eventRepository.findAll();
-        for(Event event : events) {
-            LOGGER.info(event);
-        }
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-        init();
     }
 }
--- application-stub/src/main/java/sample/rest/EventRestController.java
+++ application-stub/src/main/java/sample/rest/EventRestController.java
@@ -1,16 +1,21 @@
 package sample.rest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sample.domain.Event;
 import sample.repository.EventRepository;
 
 import java.util.List;
 
+import static sample.domain.builder.EventBuilder.event;
+
 @RestController
 public class EventRestController {
+
+    private static final Log LOGGER = LogFactory.getLog(EventRestController.class);
 
     private final EventRepository eventRepository;
 
@@ -20,7 +25,38 @@ public class EventRestController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/events")
-    public List<Event> events(){
+    public List<Event> events() {
         return eventRepository.findAll();
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/init/{eventNumber}")
+    public String init(@PathVariable Long eventNumber) {
+        eventRepository.deleteAll();
+        for (int i = 0; i < eventNumber; i++) {
+
+            Event event = event()
+                    .withDescription("This is meeting in Bar number " + i)
+                    .withName("Let's meet again (nr ( " + i + ")")
+                    .withMeetUpDate(DateTime.now().plusDays(i).plusHours(i).plusMinutes(i))
+                    .withUser()
+                    .withName("Bożydar " + i)
+                    .withEmail("dar.boży." + i + "@rodzice.przegrali.zaklad.pl")
+                    .endUser()
+                    .withPlace()
+                    .withPlace("Super miejscowa " + i)
+                    .endPlace()
+                    .build();
+
+            eventRepository.save(event);
+
+        }
+
+        List<Event> events = eventRepository.findAll();
+        for (Event event : events) {
+            LOGGER.info(event);
+        }
+
+        return "OK";
     }
 }
